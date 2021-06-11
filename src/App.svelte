6d4f961dc790b64d6016e0d5a030cfad905f622a
Justin <faakjustin2@gmail.com>
2021-06-11 20:36:09 +0200

    initial setup

---
+++ src/App.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+
+</script>
+
+<main>
+	<h1>Hello World</h1>
+</main>
+
+<style lang="scss">
+	@import url('https://fonts.googleapis.com/css2?family=Work+Sans:wght@100;200;300;400;500;600;700;800;900&display=swap');
+
+	:global(*) {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+	}
+
+	:global(body) {
+		font-family: "Work Sans", sans-serif;
+		color: #191919;
+		background: #FFFFFF;
+	}
+
+	:global(button) {
+		&:focus {
+			outline: none;
+		}
+	}
+</style>
